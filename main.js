407401a7b80f32d46037caf62b0933924331e2a0
mf-3d <mf72cli120@gmail.com>
2022-02-04 22:25:32 +0900

    2022-02-04 Update

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const {app, BrowserWindow, dialog, Menu, ipcMain, BrowserView} = require('electron');
 const Store = require('electron-store');
 const store = new Store();
+const contextMenu = require('electron-context-menu');
 let win;
 let bv;
 let menu;
@@ -11,7 +12,6 @@ var viewY = 45;
 
 let winSize;
 
-
 function nw(){
   win=new BrowserWindow({
     width: store.get('width', 800),
@@ -20,7 +20,8 @@ function nw(){
     minHeight: 400,
     icon: `${__dirname}/icon.png`,
     frame: false,
-    toolbar: false
+    toolbar: false,
+    title: 'Flune Browser'
   });
 
   winSize = win.getSize();
@@ -32,14 +33,12 @@ function nw(){
       }
   });
 
-  bv.webContents.openDevTools();
   menu=new BrowserView({
     width: `${win.getSize()[0]}`,
     webPreferences: {
       preload: `${__dirname}/src/preload.js`
     }
   });
-
   // menu.webContents.openDevTools();
 
   win.setBrowserView(bv);
@@ -83,6 +82,7 @@ app.on('ready',()=>{
   if(process.argv[2] == '--dev'){
     console.log('develop!');
     viewY = 100;
+    menu.webContents.openDevTools();
   }
 });
 app.on('window-all-closed', ()=>app.quit());
@@ -95,28 +95,36 @@ ipcMain.handle('open_url', (event, data) => {
   if(data.match(url_regexp)) {
     console.log('🤔')
     bv.webContents.loadURL(data);
+    menu.webContents.send('page_changed', data);
   }
-  if(data.match(no_http_url_regexp)) {
-    if(data.match('file:///')) {
-      bv.webContents.loadURL(data);
-    }
-    else {
-      console.log('😔')
-      bv.webContents.loadURL('https://' + data);
-    }
+  else if(data.match('://')){
+    bv.webContents.loadURL(data);
+    menu.webContents.send('page_changed', data);
   }
   else {
     bv.webContents.loadURL('https://www.google.com/search?q=' + data);
+    menu.webContents.send('page_changed', data);
   }
   console.log(data);
 });
 
-ipcMain.handle('page_back', (event, data) => {
+ipcMain.handle('pageback', (event, data) => {
   bv.webContents.goBack();
+  menu.webContents.send('page_changed', '');
+});
+
+ipcMain.handle('bv_url', (event, data) => {
+  return (bv.webContents.getURL());
+});
+
+ipcMain.handle('pageforward', (event, data) => {
+  bv.webContents.goForward();
+  menu.webContents.send('page_changed', '');
 });
 
 ipcMain.handle('open_home', (event, data) => {
   bv.webContents.loadURL(`file://${__dirname}/src/resource/home.html`);
+  menu.webContents.send('page_changed', '');
 });
 
 ipcMain.handle('close', (event, data) => {
@@ -140,45 +148,147 @@ ipcMain.handle('maxmin', (event, data) => {
   }
 });
 
-// let menu=Menu.buildFromTemplate([
-//   {
-//     label: 'ファイル',
-//     submenu: [
-//       {
-//         label: '上書き保存',
-//         accelerator: 'CmdOrCtrl+S',
-//         click: ()=>{
-//           if (filepath == ''){
-//             win.webContents.executeJavaScript(`
-//               node.showSaveDialog(document.getElementsByTagName('textarea')[0].value)
-//             `);
-//           } else if (filepath != '') {
-//             win.webContents.executeJavaScript(`
-//               node.overwriteSave(document.getElementsByTagName('textarea')[0].value)
-//             `)
-//           }
-//         }
-//       },
-//       {
-//         label: '名前を付けて保存',
-//         accelerator: 'CmdOrCtrl+Shift+S',
-//         click: ()=>{
-//           win.webContents.executeJavaScript(`
-//             node.showSaveDialog(document.getElementsByTagName('textarea')[0].value)
-//           `);
-//         }
-//       },
-//       {
-//         label: '開く',
-//         accelerator: 'CmdOrCtrl+O',
-//         click: ()=>{
-//           win.webContents.executeJavaScript(`
-//             node.showOpenDialog();
-//           `)
-//         }
-//       }
-//     ]
-//   }
-// ])
+const template = Menu.buildFromTemplate([
+  ...(is_mac ? [{
+      label: 'Flune Browser',
+      submenu: [
+        {
+          accelerator: 'CmdOrCtrl+Alt+A',
+          click: ()=>{
+            dialog.showMessageBox(null, {
+              type: 'info',
+              icon: './src/icon.png',
+              title: 'Flune Browserについて',
+              message: 'Flune Browserについて',
+              detail: `Flune Browser 1.0.0 Alpha 3
+                バージョン: 1.0.0 Alpha 3
+                開発者: mf7cli
+                
+                Copyright 2022 mf7cli.`
+              }
+            )
+          }, 
+          label:`Flune Browserについて` 
+        },
+        {type:'separator'},
+        {role:'services',   label:'サービス'},
+        {type:'separator'},
+        {role:'hide',       label:`Flune Browserを隠す`},
+        {role:'hideothers', label:'ほかを隠す'},
+        {role:'unhide',     label:'すべて表示'},
+        {type:'separator'},
+        {role:'quit',       label:`Flune Browserを終了`}
+      ]
+    }] : []),
+  {
+    label: 'ファイル',
+    submenu: [
+      is_mac ? {role:'close', label:'ウィンドウを閉じる'} : {role:'quit', label:'終了'}
+    ]
+  },
+  {
+    label: '編集',
+    submenu: [
+      {role:'undo',  label:'元に戻す'},
+      {role:'redo',  label:'やり直す'},
+      {type:'separator'},
+      {role:'cut',   label:'切り取り'},
+      {role:'copy',  label:'コピー'},
+      {role:'paste', label:'貼り付け'},
+      ...(is_mac ? [
+          {role:'pasteAndMatchStyle', label:'ペーストしてスタイルを合わせる'},
+          {role:'delete',    label:'削除'},
+          {role:'selectAll', label:'すべてを選択'},
+          {type:'separator' },
+          {
+            label: 'スピーチ',
+            submenu: [
+              {role:'startSpeaking', label:'読み上げを開始'},
+              {role:'stopSpeaking',  label:'読み上げを停止'}
+            ]
+          }
+        ] : [
+          {role:'delete',    label:'削除'},
+          {type:'separator'},
+          {role:'selectAll', label:'すべてを選択'}
+        ])
+     ]
+  },
+  {
+    label: '表示',
+    submenu: [
+      {
+        accelerator: 'Shift+R',
+        click:()=>{
+          bv.webContents.reload();
+        },         
+        label:'再読み込み'
+      },
+      {
+        accelerator: 'Shift+Alt+R',
+        click:()=>{
+          bv.webContents.reloadIgnoringCache();
+        },         
+        label:'強制的に再読み込み'
+      },
+      {
+        accelerator: 'F12',
+        click:()=>{
+          bv.webContents.openDevTools();
+        }, 
+        label:'開発者ツールを表示'
+      },
+      {type:'separator'},
+      {role:'resetZoom',      label:'実際のサイズ'},
+      {role:'zoomIn',         label:'拡大'},
+      {role:'zoomOut',        label:'縮小'},
+      {type:'separator'},
+      {role:'togglefullscreen', label:'フルスクリーン'}
+    ]
+  },
+  {
+    label: 'ウィンドウ',
+    submenu: [
+      {role:'minimize', label:'最小化'},
+      {role:'zoom',     label:'ズーム'},
+      ...(is_mac ? [
+           {type:'separator'} ,
+           {role:'front',  label:'ウィンドウを手前に表示'},
+           {type:'separator'},
+           {role:'window', label:'ウィンドウ'}
+         ] : [
+           {role:'close',  label:'閉じる'}
+         ])
+    ]
+  },
+  {
+    label:'ヘルプ',
+    submenu: [
+      {label:`Flune Browser ヘルプ`},    // ToDo
+      ...(is_mac ? [ ] : [
+        {type:'separator'} ,
+        {
+          accelerator: 'CmdOrCtrl+Alt+A',
+          click: ()=>{
+            dialog.showMessageBox(null, {
+              type: 'info',
+              icon: './src/icon.png',
+              title: 'Flune Browserについて',
+              message: 'Flune Browserについて',
+              detail: `Flune Browser 1.0.0 Alpha 3
+                バージョン: 1.0.0 Alpha 3
+                開発者: mf7cli
+                
+                Copyright 2022 mf7cli.`
+              }
+            )
+          }, 
+          label:`Flune Browserについて` 
+        }
+      ])
+    ]
+  }
+]);
 
-// Menu.setApplicationMenu(menu);
+// メニューを適用する
+Menu.setApplicationMenu(template);
--- src/preload.js
+++ src/preload.js
@@ -2,11 +2,15 @@ const { contextBridge, ipcRenderer } = require('electron')
 
 contextBridge.exposeInMainWorld('api', {
     open_url: async (data) => await ipcRenderer.invoke('open_url', data),
-    page_back: async (data) => await ipcRenderer.invoke(' page_back', data),
+    pageback: async (data) => await ipcRenderer.invoke('pageback', data),
+    pageforward: async (data) => await ipcRenderer.invoke('pageforward', data),
     open_home: async (data) => await ipcRenderer.invoke('open_home', data),
     close: async (data) => await ipcRenderer.invoke('close', data),
     maximize: async (data) => await ipcRenderer.invoke('maximize', data),
     minimize: async (data) => await ipcRenderer.invoke('minimize', data),
-    maxmin: async (data) => await ipcRenderer.invoke('maxmin', data)
+    maxmin: async (data) => await ipcRenderer.invoke('maxmin', data),
+    bv_url: async (data) => await ipcRenderer.invoke('bv_url', data),
+
+    on: (channel, callback) => ipcRenderer.on(channel, (event, argv)=>callback(event, argv))
   }
 );
