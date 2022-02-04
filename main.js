b613156eadf1d958d344a949e8602768a747a513
mf-3d <mf72cli120@gmail.com>
2022-02-04 12:55:31 +0900

    2022-02-04 Update
    Home Button.

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ let menu;
 const is_windows = process.platform==='win32';
 const is_mac = process.platform==='darwin';
 const is_linux = process.platform==='linux';
+var viewY = 45;
 
 let winSize;
 
@@ -25,17 +26,25 @@ function nw(){
   winSize = win.getSize();
 
   bv=new BrowserView({
-      width: `${win.getSize()[0]}`
+      width: `${win.getSize()[0]}`,
+      webPreferences: {
+        preload: `${__dirname}/src/preload.js`
+      }
   });
+
+  bv.webContents.openDevTools();
   menu=new BrowserView({
     width: `${win.getSize()[0]}`,
     webPreferences: {
       preload: `${__dirname}/src/preload.js`
     }
   });
+
+  // menu.webContents.openDevTools();
+
   win.setBrowserView(bv);
   win.addBrowserView(menu);
-  menu.setBounds({ x: 0, y: 0, width: winSize[0], height: 45 });
+  menu.setBounds({ x: 0, y: 0, width: winSize[0], height: viewY });
   if(is_mac) {
     menu.webContents.loadURL(`file://${__dirname}/src/index_mac.html`);
   }
@@ -43,17 +52,11 @@ function nw(){
   else if(is_windows) {
     menu.webContents.loadURL(`file://${__dirname}/src/index_win.html`);
   }
-  
-  // bv.webContents.insertCSS(`
-  //   body {
-  //       background-color: #ffffff;
-  //   }`
-  // )
 
-  bv.setBounds({ x: 0, y: 45, width: winSize[0], height: winSize[1] - 45 });
+  bv.setBounds({ x: 0, y: viewY, width: winSize[0], height: winSize[1] - viewY });
 
   // bv.webContents.loadURL(`file://${__dirname}/src/resource/home.html`);
-  bv.webContents.loadURL(`https://mf7cli.ml`);
+  bv.webContents.loadURL(`file://${__dirname}/src/resource/home.html`);
 
   win.webContents.on('close',()=>{
     store.set('width', win.getSize()[0]);
@@ -62,8 +65,15 @@ function nw(){
 
   win.on('resize', () => {
     winSize = win.getSize();
-    menu.setBounds({ x: 0, y: 0, width: winSize[0], height: 45 });
-    bv.setBounds({ x: 0, y: 45, width: winSize[0], height: winSize[1] - 45 });
+    menu.setBounds({ x: 0, y: 0, width: winSize[0], height: viewY });
+    bv.setBounds({ x: 0, y: viewY, width: winSize[0], height: winSize[1] - viewY });
+  });
+
+  bv.webContents.on('did-start-loading',()=>{});
+  bv.webContents.on('did-stop-loading',()=>{
+    // 盗人ブルートしてきた
+    console.log(bv.webContents.getURL())
+    menu.webContents.executeJavaScript(`document.getElementById('url').value = '${bv.webContents.getURL()}'`)
   });
 }
 
@@ -71,7 +81,8 @@ app.on('ready',()=>{
   nw();
   console.log(process.argv[2])
   if(process.argv[2] == '--dev'){
-    console.log('develop!')
+    console.log('develop!');
+    viewY = 100;
   }
 });
 app.on('window-all-closed', ()=>app.quit());
@@ -79,20 +90,33 @@ app.on('activate',()=>{if (win === null) nw});
 
 // IPC
 ipcMain.handle('open_url', (event, data) => {
-  url_regexp = /(https?|http):\/\/([\w\/:%#\$&\?\(\)~\.=\+\-]+)/g;
+  url_regexp = /(https:?|http:?|file:\/)\/\/([\w\/:%#\$&\?\(\)~\.=\+\-]+)/g;
   no_http_url_regexp = /([\w\/:%#\$&\?\(\)~\.=\+\-]+)/g;
   if(data.match(url_regexp)) {
     console.log('🤔')
     bv.webContents.loadURL(data);
   }
-  // if(data.match(no_http_url_regexp)) {
-  //   console.log('😔')
-  //   bv.webContents.loadURL(data);
-  // }
+  if(data.match(no_http_url_regexp)) {
+    if(data.match('file:///')) {
+      bv.webContents.loadURL(data);
+    }
+    else {
+      console.log('😔')
+      bv.webContents.loadURL('https://' + data);
+    }
+  }
   else {
     bv.webContents.loadURL('https://www.google.com/search?q=' + data);
   }
   console.log(data);
+});
+
+ipcMain.handle('page_back', (event, data) => {
+  bv.webContents.goBack();
+});
+
+ipcMain.handle('open_home', (event, data) => {
+  bv.webContents.loadURL(`file://${__dirname}/src/resource/home.html`);
 });
 
 ipcMain.handle('close', (event, data) => {
--- src/preload.js
+++ src/preload.js
@@ -2,6 +2,8 @@ const { contextBridge, ipcRenderer } = require('electron')
 
 contextBridge.exposeInMainWorld('api', {
     open_url: async (data) => await ipcRenderer.invoke('open_url', data),
+    page_back: async (data) => await ipcRenderer.invoke(' page_back', data),
+    open_home: async (data) => await ipcRenderer.invoke('open_home', data),
     close: async (data) => await ipcRenderer.invoke('close', data),
     maximize: async (data) => await ipcRenderer.invoke('maximize', data),
     minimize: async (data) => await ipcRenderer.invoke('minimize', data),
