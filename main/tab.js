c5ea7c09375e1daf659adaf3e45b7847e3b7df4c
mf7cli <84224913+Mf-3d@users.noreply.github.com>
2022-08-13 12:44:44 +0900

    [add] userAgentをChrome化しましょう

--- main/tab.js
+++ main/tab.js
@@ -144,7 +144,15 @@ module.exports = class {
 
         this.setTitle(index);
       });
+
+      let userAgent = bv[index].webContents.getUserAgent()
+        .replace('flune-browser', 'Chrome')
+        .replace(/Electron\/[0-9 | .]/, '')
+        .replace(`Chrome/${electron.app.getVersion()}`, '');
     
+      bv[index].webContents.setUserAgent(userAgent);
+
+      console.log(`UA: ${userAgent}`);
       
       bv[index].webContents.on('media-started-playing', () => {
         win.webContents.send('each');
