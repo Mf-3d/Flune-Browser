d7b50a831bb7d4c95b0fa5bd92cd71fd13beb864
mf7cli <84224913+Mf-3d@users.noreply.github.com>
2025-02-17 14:29:33 +0900

    ⛄ 設定関連のAPIを変更

--- src/preload/browser.ts
+++ src/preload/browser.ts
@@ -1,8 +1,8 @@
 import { contextBridge, ipcRenderer } from "electron";
 
 contextBridge.exposeInMainWorld("flune", {
-  getVersion: () => {
-    return ipcRenderer.invoke("flune.ver"); // バージョン取得
+  getVersion: async () => {
+    return await ipcRenderer.invoke("flune.ver"); // バージョン取得
   },
   load: (word: string) => {
     ipcRenderer.invoke("tab.load", undefined, word); // ページをロードする
--- src/preload/settings.ts
+++ src/preload/settings.ts
@@ -1,21 +1,21 @@
 import { contextBridge, ipcRenderer } from "electron";
 
 contextBridge.exposeInMainWorld("fluneSettings", {
-  getVersion: () => {
-    return ipcRenderer.invoke("flune.ver"); // バージョン取得
+  getVersion: async () => {
+    return await ipcRenderer.invoke("flune.ver"); // バージョン取得
   },
   store: {
-    get: (key: string) => {
-      return ipcRenderer.invoke("flune.store.config.get", key); // 項目を取得
+    get: async (key: string) => {
+      return await ipcRenderer.invoke("flune.store.config.get", key); // 項目を取得
     },
-    getAll: () => {
-      return ipcRenderer.invoke("flune.store.config.get-all"); // コンフィグをすべて取得
+    getAll: async () => {
+      return await ipcRenderer.invoke("flune.store.config.get-all"); // コンフィグをすべて取得
     },
     save: (key: string, value?: any) => {
-      return ipcRenderer.invoke("flune.store.config.save", key, value); // 項目を保存
+      ipcRenderer.invoke("flune.store.config.save", key, value); // 項目を保存
     },
     saveAll: (value?: any) => {
-      return ipcRenderer.invoke("flune.store.config.save-all", value); // コンフィグをすべて保存
+      ipcRenderer.invoke("flune.store.config.save-all", value); // コンフィグをすべて保存
     },
   }
 });
