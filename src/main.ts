19a03a5af37b4113d681b296858c02919a936793
mf7cli <84224913+Mf-3d@users.noreply.github.com>
2025-05-03 18:25:19 +0900

    🐛 Intel Macでエラーが発生する問題を修正

--- src/main.ts
+++ src/main.ts
@@ -16,8 +16,22 @@ function nw() {
     base?.tabManager?.newTab();
   });
   
-  if (!app.isPackaged) base.nav.webContents.openDevTools();
+  if (!app.isPackaged) base.nav.webContents.openDevTools({
+    mode: "detach"
+  });
 }
+
+function isArchitectureIntel(): boolean {
+  const f = new Float32Array(1);
+  const u8 = new Uint8Array(f.buffer);
+  f[0] = Infinity;
+  f[0] = f[0] - f[0];
+
+  return u8[3] === 255;
+}
+
+// Intel Macでエラーが出るのを回避する
+if (process.platform === "darwin" && isArchitectureIntel()) app.disableHardwareAcceleration();
 
 app.on("ready", () => {
   event.send("init");
