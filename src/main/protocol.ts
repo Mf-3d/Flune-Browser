6ec5ec6415dc28f2a484c1206ebbd49dd1e698d0
mf7cli <84224913+Mf-3d@users.noreply.github.com>
2025-12-13 13:05:00 +0900

    ♲ プロトコルの設計を変更
    - ⚡ protocol.getFilePathByPath()を追加

--- src/main/protocol.ts
+++ src/main/protocol.ts
@@ -15,8 +15,25 @@ export class Protocol {
       style: path.join(__dirname, "..", "renderer", "style"),
       assets: path.join(__dirname, "..", "assets"),
       error: path.join(__dirname, "..", "renderer", "browser", "error"),
+      // "foo.bar": path.join(__dirname, "..", "renderer", "browser", "foo", "bar"),
     };
+
   readonly event = new Event();
+
+  readonly paths: {
+    [key: string]: {
+      pathWithProtocol: string,
+      path: string,
+      filePath: string
+    },
+  } = Object.fromEntries(Object.entries(this.pathToServe).map(([key, value]) => [
+    key,
+   {
+      pathWithProtocol: `${this.name}://${key.replace(/\./g, "/")}`,
+      path: `/${key.replace(/\./g, "/")}`,
+      filePath: this.pathToServe[key]
+    }
+  ]));
 
   constructor(name: string = "flune") {
     this.name = name;
@@ -28,21 +45,6 @@ export class Protocol {
       switch (Url) {
         case "/ping": {
           return new Response("pong!", {
-            headers: { "content-type": "text/html" }
-          });
-        }
-        case "/home": {
-          return net.fetch(pathToFileURL(this.pathToServe.home).toString(), {
-            headers: { "content-type": "text/html" }
-          });
-        }
-        case "/settings": {
-          return net.fetch(pathToFileURL(this.pathToServe.settings).toString(), {
-            headers: { "content-type": "text/html" }
-          });
-        }
-        case "/version": {
-          return net.fetch(pathToFileURL(this.pathToServe.version).toString(), {
             headers: { "content-type": "text/html" }
           });
         }
@@ -63,6 +65,14 @@ export class Protocol {
           return net.fetch(pathToFileURL(this.pathToServe.error + Url.slice(6)).toString());
         }
         default: {
+          if (Object.values(this.paths).map(item => item.path)) {
+            return net.fetch(
+              pathToFileURL(this.getFilePathByPath(Url) || "")
+              .toString(), {
+                headers: { "content-type": "text/html" }
+              });
+          }
+
           return new Response(`not found: <pre>${Url}</pre>`, {
             status: 404,
             headers: { "content-type": "text/html" }
@@ -70,6 +80,14 @@ export class Protocol {
         }
       }
     });
+  }
+
+  getFilePathByPath(
+    path: string
+  ): string | undefined {
+    return Object.values(this.paths)
+      .find(route => route.path === path)
+      ?.filePath;
   }
 };
 
