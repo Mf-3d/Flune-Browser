4e8bf20550d253f48c5f67ffd5e98d6a9f11735d
mf7cli <84224913+Mf-3d@users.noreply.github.com>
2025-12-18 18:13:57 +0900

    👽️ Electronの"webFrame.routingId"が非推奨になったので修正

--- src/preload/browser.ts
+++ src/preload/browser.ts
@@ -8,11 +8,12 @@ contextBridge.exposeInMainWorld("flune", {
     return await ipcRenderer.invoke("flune.get-versions"); // ElectronやChromeのバージョンも取得
   },
   toggleBookmark: (data: {
-    session: number,
+    session: string,
     url: string,
     title: string,
   }) => {
-    if (data.session === webFrame.routingId) ipcRenderer.invoke("tab.toggle-bookmark", {
+    // 正しいセッションから実行されているか判定
+    if (data.session === webFrame.frameToken) ipcRenderer.invoke("tab.toggle-bookmark", {
       url: data.url,
       title: data.title
     });
@@ -22,10 +23,11 @@ contextBridge.exposeInMainWorld("flune", {
   },
 });
 
+// ナビゲーションバーから、ブラウザビューがブックマークを追加するように指示する。
 ipcRenderer.on("nav.toggle-bookmark", (event) => {
   webFrame.executeJavaScript(`
   flune.toggleBookmark({
-    session: ${webFrame.routingId},
+    session: ${webFrame.frameToken},
     url: location.href,
     title: document.head.getElementsByTagName('title')[0].innerText,
   });
