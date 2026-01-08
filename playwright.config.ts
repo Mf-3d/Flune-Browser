91e01a7ec70db9aef30cdc7fb54fd5ae0514361d
mf7cli <84224913+Mf-3d@users.noreply.github.com>
2026-01-08 16:55:33 +0900

    🎨 playwright.config.tsをフォーマット

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,13 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true});
 
 const config = {
-    testDir: "tests/e2e",
-    timeout: 30 * 1000,
-    use: {
-        headless: true,
-    },
-    expect: {
-        timeout: 5000
-    },
+  testDir: "tests/e2e",
+  timeout: 30 * 1000,
+  use: {
+    headless: true,
+  },
+  expect: {
+    timeout: 5000
+  },
 };
 exports.default = config;
