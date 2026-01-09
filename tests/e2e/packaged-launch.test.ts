7508ecebd62a7d499e096f51f12e439c7224ac4d
mf7cli <84224913+Mf-3d@users.noreply.github.com>
2026-01-09 19:12:25 +0900

    🚧 テストコードのバグを検証中１１

--- tests/e2e/packaged-launch.test.ts
+++ tests/e2e/packaged-launch.test.ts
@@ -13,8 +13,10 @@ test("packaged app launches and survives", async ({}, testInfo) => {
     const executablePath = process.env.ELECTRON_EXECUTABLE_PATH ? path.resolve(process.env.ELECTRON_EXECUTABLE_PATH) : undefined;
     console.info("executablePath:", executablePath);
     if (executablePath) {
-      console.info(fs.readdirSync("./dist/"))
-      if (fs.existsSync("./dist/")) console.info(fs.readdirSync("./dist/win-packed"));
+      console.info(fs.readdirSync("./dist/", {
+        recursive: true
+      }))
+      if (fs.existsSync("./dist/win-unpacked/")) console.info(fs.readdirSync("./dist/win-unpacked/"));
       console.info("File exists:", fs.existsSync(executablePath));
     }
 
