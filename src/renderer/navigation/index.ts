35f94a8f6025118ae974a92ababb979aeb66f157
mf7cli <84224913+Mf-3d@users.noreply.github.com>
2026-03-06 13:16:52 +0900

    ♻️ レンダラー側のナビゲーションのスクリプトを最適化

--- src/renderer/navigation/index.ts
+++ src/renderer/navigation/index.ts
@@ -1,13 +1,15 @@
-import { registerInputEvents } from "./ui/input-events";
 import lucide from "../script/icons.js";
+import { registerInputEvents } from "./ui/input-events";
 import { registerClickEvents } from "./ui/click-events";
 import { registerNavigationEvents } from "./events/navigation-events";
 import { registerTabEvents } from "./events/tab-events";
 
 
 window.addEventListener("load", () => {
-  if (!window.flune.navigation) return;
+  navigationInit();
+});
 
+function navigationInit() {
   lucide.createIcons();
 
   registerClickEvents();
@@ -15,100 +17,8 @@ window.addEventListener("load", () => {
   registerNavigationEvents();
   registerTabEvents();
 
-  // window.flune.navigation.tab.onCreated((_, tab) => {
-  //   const newButton = tabContainer.querySelector(".new-button")!;
-
-  //   const element = document.createElement("span");
-  //   element.draggable = true;
-    
-  //   element.setAttribute("data-id", tab.id);
-
-  //   element.innerHTML = `
-  //   <img src="" class="favicon" onerror="this.src='/image/tab-no-favicon.png';"/>
-  //   <a href="#" class="loading disabled">
-  //     <i data-lucide="loader-circle"></i>
-  //   </a>
-  //   <p class="title">${tab.title}</p>
-  //   <span class="right">
-  //     <a href="#" class="downloading disabled">
-  //       <i data-lucide="download"></i>
-  //     </a>
-  //     <a href="#" class="audible disabled">
-  //       <i data-lucide="volume-2"></i>
-  //     </a>
-  //   </span>
-  //   <a href="javascript:window.flune.navigation?.tab.remove('${tab.id}')" class="close-button right">
-  //     <i data-lucide="x"></i>
-  //   </a>
-  //   `;
-
-  //   if (!tab.beforeTabId) {
-  //     newButton.before(element); // 一番右に追加
-  //   } else {
-  //     tabContainer.querySelector(`:scope > span[data-id="${tab.beforeTabId}"]`)?.after(element);
-  //   }
-    
-  //   if (tab.active) activateTab(tab.id);
-
-  //   lucide.createIcons();
-
-  //   each();
-  // });
-
-  // window.flune.navigation.tab.onRemoved((_, id) => {
-  //   const tabElements = tabContainer.querySelectorAll(":scope > span");
-
-  //   tabElements.forEach(tabElement => {
-  //     if (tabElement.getAttribute("data-id") === id) tabElement.remove();
-  //   });
-
-  //   each();
-  // });
-
-  // window.flune.navigation.tab.onUpdated((_, tab) => {
-  //   const tabElements = tabContainer.querySelectorAll(":scope > span");
-
-  //   tabElements.forEach((tabElement) => {
-  //     if (tabElement.getAttribute("data-id") !== tab.id) return;
-
-  //     if (tab.title !== undefined) {
-  //       console.info("(change-state) title:", tab.title);
-  //       const titleElement = tabElement.querySelector("p.title")! as HTMLElement;
-  //       titleElement.innerText = tab.title;
-  //     }
-
-  //     if (tab.favicon !== undefined) {
-  //       console.info("(change-state) favicon:", tab.favicon);
-  //       const faviconElement = tabElement.querySelector("img.favicon")! as HTMLElement;
-  //       faviconElement.setAttribute("src", tab.favicon);
-  //     }
-
-  //     if (tab.isLoading !== undefined) {
-  //       console.info("(change-state) loading:", tab.isLoading);
-  //       const loadingElement = tabElement.querySelector("a.loading")! as HTMLElement;
-
-  //       if (tab.isLoading) loadingElement.classList.remove("disabled");
-  //       else loadingElement.classList.add("disabled");
-  //     }
-
-  //     if (tab.isAudible !== undefined) {
-  //       console.info("(change-state) audible:", tab.isAudible);
-  //       const audibleElement = tabElement.querySelector("a.audible")! as HTMLElement;
-
-  //       if (tab.isAudible) audibleElement.classList.remove("disabled");
-  //       else audibleElement.classList.add("disabled");
-  //     }
-  //   });
-
-  //   if (tab.active !== undefined) {
-  //     activateTab(tab.id);
-  //   }
-  // });
-
-  each();
-});
-
-
+  each(); // 状態更新
+}
 
 function each() {
   const tabContainer = document.getElementById("tabs")!;
