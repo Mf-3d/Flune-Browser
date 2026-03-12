81cbe27aa4e93a7e5dcacd7ba22165df5b502e60
mf7cli <84224913+Mf-3d@users.noreply.github.com>
2026-03-12 15:26:29 +0900

    💡 コメントを削除

--- src/preload/settings.ts
+++ src/preload/settings.ts
@@ -2,29 +2,6 @@ import { SettingsAPI } from "../shared/types/preload-api";
 import { IPC_INVOKE } from "../shared/ipc/channels";
 import { ipcRenderer } from "electron";
 
-// contextBridge.exposeInMainWorld("fluneSettings", {
-//   getVersion: async () => {
-//     return await ipcRenderer.invoke("flune.get-version"); // バージョン取得
-//   },
-//   getVersions: async () => {
-//     return await ipcRenderer.invoke("flune.get-versions"); // ElectronやChromeのバージョンも取得
-//   },
-//   store: {
-//     get: async (key: string) => {
-//       return await ipcRenderer.invoke("flune.store.config.get", key); // 項目を取得
-//     },
-//     getAll: async () => {
-//       return await ipcRenderer.invoke("flune.store.config.get-all"); // コンフィグをすべて取得
-//     },
-//     set: (key: string, value?: any) => {
-//       ipcRenderer.invoke("flune.store.config.save", key, value); // 項目を保存
-//     },
-//     setAll: (value?: any) => {
-//       ipcRenderer.invoke("flune.store.config.save-all", value); // コンフィグをすべて保存
-//     },
-//   }
-// });
-
 export function isSettingsPage() {
   const isDev = !process.argv.includes("--is-packaged=true") && !!process.env.ELECTRON_RENDERER_URL;
 
