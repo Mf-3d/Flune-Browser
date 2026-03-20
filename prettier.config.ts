476002ceec13ad7ecaf7db7ae1dfd96157816908
mf7cli <84224913+Mf-3d@users.noreply.github.com>
2026-03-21 00:57:44 +0900

    🔧 prettier.config.tsを追加

---
+++ prettier.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "prettier";
+
+const config: Config = {
+  printWidth: 90,
+  tabWidth: 2,
+  singleQuote: false,
+  semi: true,
+  trailingComma: "es5",
+  singleAttributePerLine: true,
+};
+
+export default config;
